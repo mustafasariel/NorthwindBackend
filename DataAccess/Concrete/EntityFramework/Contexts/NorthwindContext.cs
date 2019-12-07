61845327d360445fb42f0756114caa0c642afaad
mustafa sarıel <mustafasariel@hotmail.com>
2019-12-08 02:02:49 +0300

    generic repository

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=msariel\SQLEXPRESS;Database=Northwind;Integrated security=true;");
+        }
+
+      
+    }
+}
--- WebApplicationAPI/Startup.fs
+++ WebApplicationAPI/Startup.fs
@@ -1,4 +1,4 @@
-namespace WebAPI
+namespace WebApplicationAPI
 
 open System
 open System.Collections.Generic
@@ -10,6 +10,7 @@ open Microsoft.AspNetCore.HttpsPolicy;
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 
 type Startup private () =
     new (configuration: IConfiguration) as this =
@@ -19,16 +20,20 @@ type Startup private () =
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
-        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
+        services.AddControllers() |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
+    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
         if (env.IsDevelopment()) then
             app.UseDeveloperExceptionPage() |> ignore
-        else
-            app.UseHsts() |> ignore
 
         app.UseHttpsRedirection() |> ignore
-        app.UseMvc() |> ignore
+        app.UseRouting() |> ignore
+
+        app.UseAuthorization() |> ignore
+
+        app.UseEndpoints(fun endpoints ->
+            endpoints.MapControllers() |> ignore
+            ) |> ignore
 
     member val Configuration : IConfiguration = null with get, set
