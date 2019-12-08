3c8752460efac19b3f75f90b939009cc29c34dba
mustafa sarıel <mustafasariel@hotmail.com>
2019-12-08 20:57:24 +0300

    business katmanını yazarken repository patterni uygulanmaz. repository dal katmanında uygulanır.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+   public interface IProductService
+    {
+        List<Product> GetList();
+        List<Product> GetListByCategory(int CategoryId);
+
+        void Delete(Product product);
+        void Update(Product product);
+
+        Product GetById(int ProductId);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetListByCategory(int CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
